35fcc8d7f747a38233140f91f8d7bbb7b135babe
Byron Reyes <reyesbyron0503@gmail.com>
2024-03-12 22:17:38 +0800

    First commit

---
+++ TodoList/Controllers/TodoController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
+
+public class TodoController : Controller
+{
+    private static List<TodoItem> todoList = new List<TodoItem>();
+
+    public IActionResult Index()
+    {
+        return View(todoList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(TodoItem todoItem)
+    {
+        todoItem.Id = todoList.Count + 1;
+        todoList.Add(todoItem);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult MarkAsCompleted(int id)
+    {
+        var todoItem = todoList.Find(item => item.Id == id);
+        if (todoItem != null)
+        {
+            todoItem.IsCompleted = true;
+        }
+        return RedirectToAction("Index");
+    }
+}
+
+
